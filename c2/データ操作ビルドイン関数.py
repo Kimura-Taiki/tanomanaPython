1d06707296bdec104ba37768da755d9493bb6f41
Kimura-Taiki <taiguimucum850@gmail.com>
2023-07-25 19:46:08 +0900

    データ操作ビルドイン関数

---
+++ c2/データ操作ビルドイン関数.py
@@ -0,0 +1,21 @@
+    # インデント設定
+
+#     count(str)関数
+# -------------------------
+# 文字列中にstrがいくつあるかを数える関数。
+print("functions are fun".count("fun"))
+
+#     upper()関数
+# -------------------------
+# 文字列の小文字を全て大文字にする関数。
+print("blue".upper())
+
+#     reverse()関数
+# -------------------------
+# リストを逆順にする関数。
+countdown = [1, 2, 3]
+print(countdown.reverse())
+    # replace(str1, str2)関数
+# -------------------------
+# 文字列中のstr1をstr2へ置換する関数。
+print("Coding makes me happy".replace("happy", "D"))
