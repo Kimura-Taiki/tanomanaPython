fb796ea4e9feb503b7d2bcaae55b83b463e4e83f
Kimura-Taiki <taiguimucum850@gmail.com>
2023-07-25 18:03:53 +0900

    メッセージだよー

---
+++ 簡単な図形.py
@@ -0,0 +1,14 @@
+from turtle import *
+import tkinter
+# ウィンドウ作成
+root = tkinter.Tk()
+root.title("簡単な図形")
+root.minsize(640, 480)
+root.option_add("*font", ["ＭＳ Ｐゴシック", 28])
+
+    # dayo
+forward(200)
+left(90)
+forward(300)
+
+# root.mainloop()
