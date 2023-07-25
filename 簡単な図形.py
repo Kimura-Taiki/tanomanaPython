33e44e4e5f50a68d077683bb1071f556451adfe9
Kimura-Taiki <taiguimucum850@gmail.com>
2023-07-25 18:57:42 +0900

    インデント定義を変更

--- 簡単な図形.py
+++ 簡単な図形.py
@@ -5,10 +5,9 @@ root = tkinter.Tk()
 root.title("簡単な図形")
 root.minsize(640, 480)
 root.option_add("*font", ["ＭＳ Ｐゴシック", 28])
-
-    # dayo
+    # このコメント行はインデントを定義します
 forward(200)
 left(90)
 forward(300)
 
-root.mainloop()
+# root.mainloop()
