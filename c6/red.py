d74a9dba22187d9c383f2765958071d397722cfe
Kimura-Taiki <taiguimucum850@gmail.com>
2023-08-06 19:48:14 +0900

    完成版

--- c6/red.py
+++ c6/red.py
@@ -55,7 +55,52 @@ def create_stars(colours_to_create):
     return new_stars
 
 def layout_stars(stars_to_layout):
-    pass
+    number_of_gaps = len(stars_to_layout) + 1
+    gap_size = WIDTH / number_of_gaps
+    random.shuffle(stars_to_layout)
+    for index, star in enumerate(stars_to_layout):
+        new_x_pos = (index + 1) * gap_size
+        star.x = new_x_pos
 
 def animate_stars(stars_to_animate):
-    pass
+    global animations
+    for star in stars_to_animate:
+        duration = START_SPEED - current_level
+        star.anchor = ("center", "bottom")
+        animation = animate(star, duration=duration, on_finished=handle_game_over, y=HEIGHT)
+        animations.append(animation)
+
+def handle_game_over():
+    global game_over
+    game_over = True
+
+def on_mouse_down(pos):
+    global stars, current_level
+    for star in stars:
+        if star.collidepoint(pos):
+            if "red" in star.image:
+                red_star_click()
+            else:
+                handle_game_over()
+
+def red_star_click():
+    global current_level, stars, animations, game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level += 1
+        stars = []
+        animations = []
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+def display_message(heading_text, sub_heading_text):
+    screen.draw.text(heading_text, fontsize=60, center=CENTRE, color=FONT_COLOUR)
+    screen.draw.text(sub_heading_text,
+                     fontsize=30,
+                     center=(CENTRE_X, CENTRE_Y + 30),
+                     color=FONT_COLOUR)
