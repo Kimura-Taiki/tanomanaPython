7f83acebdece8ffe2de6eda32ddb28749a47d4a6
Kimura-Taiki <taiguimucum850@gmail.com>
2023-08-06 17:21:56 +0900

    レベルアップできた

--- c5/numbers.py
+++ c5/numbers.py
@@ -5,14 +5,19 @@ HEIGHT = 400
 
 dots = []
 lines = []
-
+number_of_dots = 3
 next_dot = 0
 
-for dot in range(0, 10):
-    actor = Actor("dot")
-    actor.pos = randint(20, WIDTH - 20), \
-    randint(20, HEIGHT -20)
-    dots.append(actor)
+def next_level():
+    global dots, lines, next_dot, number_of_dots
+    dots = []
+    lines = []
+    next_dot = 0
+    for dot in range(0, number_of_dots):
+        actor = Actor("dot")
+        actor.pos = randint(20, WIDTH - 20), \
+            randint(20, HEIGHT -20)
+        dots.append(actor)
 
 def draw():
     screen.fill("black")
@@ -26,13 +31,18 @@ def draw():
         screen.draw.line(line[0], line[1], (100, 0, 0))
 
 def on_mouse_down(pos):
-    pass
+    global number_of_dots
     global next_dot
     global lines
     if dots[next_dot].collidepoint(pos):
         if next_dot:
             lines.append((dots[next_dot -1].pos, dots[next_dot].pos))
         next_dot += 1
+        if next_dot == number_of_dots:
+            number_of_dots += 2
+            next_level()
     else:
         lines = []
         next_dot = 0
+
+next_level()
