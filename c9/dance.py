3c95d8d4b4c31cef0d0801c784228ff56c966b46
Kimura-Taiki <taiguimucum850@gmail.com>
2023-08-10 22:55:19 +0900

    ９章：generate_moves(真似元の準備)まで

--- c9/dance.py
+++ c9/dance.py
@@ -44,6 +44,20 @@ def draw():
         screen.draw.text("Score: " +
                          str(score), color="black",
                          topleft=(10, 10))
+        if say_dance:
+            screen.draw.text("Dance!", color="black",
+                            topleft=(CENTRE_X - 65, 150), fontsize=60)
+        if show_countdown:
+            screen.draw.text(str(count), color="black",
+                            topleft=(CENTRE_X - 8, 150), fontsize=60)
+    else:
+        screen.clear()
+        screen.blit("stage", (0, 0))
+        screen.draw.text("Score: " +
+                         str(score), color="black",
+                         topleft=(10, 10))
+        screen.draw.text("GAME OVER!", color="black",
+                         topleft=(CENTRE_X - 130, 220), fontsize=60)
     return
 
 def reset_dancer():
@@ -78,13 +92,51 @@ def update_dancer(move):
     return
 
 def display_moves():
-    pass
+    global move_list, display_list, dance_length
+    global say_dance, show_countdown, current_move
+    if display_list:
+        this_move = display_list[0]
+        display_list = display_list[1:]
+        if this_move == 0:
+            update_dancer(0)
+            clock.schedule(display_moves, 1)
+        elif this_move == 1:
+            update_dancer(1)
+            clock.schedule(display_moves, 1)
+        elif this_move == 2:
+            update_dancer(2)
+            clock.schedule(display_moves, 1)
+        elif this_move == 3:
+            update_dancer(3)
+            clock.schedule(display_moves, 1)
+    else:
+        say_dance = True
+        show_countdown = False
+    return
 
 def generate_moves():
-    pass
+    global move_list, dance_length, count
+    global show_countdown, say_dance
+    count = 4
+    move_list = []
+    say_dance = False
+    for move in range(0, dance_length):
+        rand_move = randint(0, 3)
+        move_list.append(rand_move)
+        display_list.append(rand_move)
+    show_countdown = True
+    countdown()
+    return
 
 def countdown():
-    pass
+    global count, game_over, show_countdown
+    if count > 1:
+        count -= 1
+        clock.schedule(countdown, 1)
+    else:
+        show_countdown = False
+        display_moves()
+    return
 
 def next_move():
     pass
@@ -100,6 +152,8 @@ def on_key_up(key):
     elif key == keys.LEFT:
         update_dancer(3)
     return
+
+generate_moves()
 
 def update():
     pass
