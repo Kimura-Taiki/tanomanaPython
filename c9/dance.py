027d9ade5498cede53ce6f4632f6fde5f1ebc4b5
Kimura-Taiki <taiguimucum850@gmail.com>
2023-08-10 21:53:48 +0900

    ９章：ダンサーを操縦できる

--- c9/dance.py
+++ c9/dance.py
@@ -45,3 +45,61 @@ def draw():
                          str(score), color="black",
                          topleft=(10, 10))
     return
+
+def reset_dancer():
+    global game_over
+    if not game_over:
+        dancer.image = "dancer-start"
+        up.image = "up"
+        right.image = "right"
+        down.image = "down"
+        left.image = "left"
+    return
+
+def update_dancer(move):
+    global game_over
+    if not game_over:
+        if move == 0:
+            up.image = "up-lit"
+            dancer.image = "dancer-up"
+            clock.schedule(reset_dancer, 0.5)
+        elif move == 1:
+            right.image = "right-lit"
+            dancer.image = "dancer-right"
+            clock.schedule(reset_dancer, 0.5)
+        elif move == 2:
+            down.image = "down-lit"
+            dancer.image = "dancer-down"
+            clock.schedule(reset_dancer, 0.5)
+        elif move == 3:
+            left.image = "left-lit"
+            dancer.image = "dancer-left"
+            clock.schedule(reset_dancer, 0.5)
+    return
+
+def display_moves():
+    pass
+
+def generate_moves():
+    pass
+
+def countdown():
+    pass
+
+def next_move():
+    pass
+
+def on_key_up(key):
+    global score, game_over, move_list, current_move
+    if key == keys.UP:
+        update_dancer(0)
+    elif key == keys.RIGHT:
+        update_dancer(1)
+    elif key == keys.DOWN:
+        update_dancer(2)
+    elif key == keys.LEFT:
+        update_dancer(3)
+    return
+
+def update():
+    pass
